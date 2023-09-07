7631a4199901e0b76fe7a525d3569864f28b7d1c
unknown <harikeshav.rajesh@gmail.com>
2023-09-07 17:02:31 +0530

    Added weather and bitcoin slash commands

--- bot.py
+++ bot.py
@@ -177,6 +177,41 @@ async def joke(interaction):
     joke = data['joke']
     await interaction.followup.send(f'{joke}')
 
+
+
+@client.slash_command(name='weather',description='Weather ForeCast')
+async def weather(interaction):
+    await interaction.response.defer()
+
+    r=requests.get('https://api.openweathermap.org/data/2.5/weather?lat=12.9716&lon=77.5946&appid=e2cf4011212692565243c810f489913f')
+    data=r.json()
+    def Cel(a):
+        C=a-273.15
+        return C
+    await interaction.followup.send("Bangalore Current Weather:")
+    await interaction.followup.send("Description:%s\nTemperature:%.2fC"%(data['weather'][0]['description'],Cel(data['main']['temp'])))
+    await interaction.followup.send("It feels like %.2fC"%(Cel(data['main']['feels_like'])))
+    await interaction.followup.send("The pressure is %d hPa"%(data['main']['pressure']))
+    await interaction.followup.send("The humidity is at %d percent and wind speed is %f m/s"%(data['main']['humidity'],data['wind']['speed']))
+
+
+
+@client.slash_command(name='bitcoin',description='Latest BitCoin Values')
+async def bitcoin(interaction):
+
+    await interaction.response.defer()
+
+    r=requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data=r.json()
+    
+    await interaction.followup.send(f"The current time is {data['time']['updateduk']}")
+    await interaction.followup.send(data['disclaimer'])
+    await interaction.followup.send("The current Bitcoin rate of 1%s(%s) is %s"%(data['bpi']['USD']['code'],data['bpi']['USD']['description'],data['bpi']['USD']['rate']))
+    await interaction.followup.send("The current Bitcoin rate of 1%s(%s) is %s"%(data['bpi']['GBP']['code'],data['bpi']['GBP']['description'],data['bpi']['GBP']['rate']))
+    await interaction.followup.send("The current Bitcoin rate of 1%s(%s) is %s"%(data['bpi']['EUR']['code'],data['bpi']['EUR']['description'],data['bpi']['EUR']['rate']))
+
+
+
 @client.slash_command(name='trivia',description="Random Trivia Question")
 async def trivia(interaction):
     
